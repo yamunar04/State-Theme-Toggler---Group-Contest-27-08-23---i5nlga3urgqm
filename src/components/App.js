6ff4a0e4fdfc294fa990131e7fd07f2bc9e0b009
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-27 19:21:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-import React, { useState } from "react"; import "../styles/App.css"; export default function App() { const [theme, setTheme] = useState("light"); const toggleTheme = () => { setTheme(theme === "light" ? "dark" : "light"); }; return ( <div className={`App ${theme === "light" ? "light" : "dark"}`}> <h1>Newton School</h1> <div> <form> <label>UserName</label> <input></input> <label>Password</label> <input></input> <button>Login</button> </form> </div> <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label> <input type="checkbox" id="switch" onChange={toggleTheme} /> <label htmlFor="switch" className="label"> Toggle </label> </div> ); } 
+import React, { useState } from "react";
+import "../styles/App.css";
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <div className={`App ${theme === "light" ? "light" : "dark"}`}>
+      <h1>Newton School</h1>
+      <div>
+        <form>
+          <label>UserName</label>
+          <input></input>
+          <label>Password</label>
+          <input></input>
+          <button>Login</button>
+        </form>
+      </div>
+      <label>{theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+      <input type="checkbox" id="switch" onChange={toggleTheme} />
+      <label htmlFor="switch" className="label">
+        Toggle
+      </label>
+    </div>
+  );
+}
